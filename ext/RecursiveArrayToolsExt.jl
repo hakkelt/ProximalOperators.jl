b9f6cf88186fef2ab6234cd2915ae24abea54833
Tamas Hakkel <hakkelt@gmail.com>
2026-07-02 00:54:55 +0200

    Revert incorrect prox! and gradient! wrapper fixes, keep gradient wrapper

--- ext/RecursiveArrayToolsExt.jl
+++ ext/RecursiveArrayToolsExt.jl
@@ -4,28 +4,16 @@ using ProximalOperators
 import ProximalCore: prox, prox!, gradient, gradient!
 
 (f::PrecomposedSlicedSeparableSum)(x::ArrayPartition) = f(x.x)
-function prox!(y::ArrayPartition, f::PrecomposedSlicedSeparableSum, x::ArrayPartition, gamma)
-    _, fy = prox!(y.x, f, x.x, gamma)
-    return y, fy
-end
+prox!(y::ArrayPartition, f::PrecomposedSlicedSeparableSum, x::ArrayPartition, gamma) = prox!(y.x, f, x.x, gamma)
 
 (g::SeparableSum)(xs::ArrayPartition) = g(xs.x)
-function prox!(ys::ArrayPartition, g::SeparableSum, xs::ArrayPartition, gamma::Number)
-    _, fy = prox!(ys.x, g, xs.x, gamma)
-    return ys, fy
-end
-function prox!(ys::ArrayPartition, g::SeparableSum, xs::ArrayPartition, gammas::Tuple)
-    _, fy = prox!(ys.x, g, xs.x, gammas)
-    return ys, fy
-end
+prox!(ys::ArrayPartition, g::SeparableSum, xs::ArrayPartition, gamma::Number) = prox!(ys.x, g, xs.x, gamma)
+prox!(ys::ArrayPartition, g::SeparableSum, xs::ArrayPartition, gammas::Tuple) = prox!(ys.x, g, xs.x, gammas)
 function prox(g::SeparableSum, xs::ArrayPartition, gamma=1)
     y, fy = prox(g, xs.x, gamma)
     return ArrayPartition(y), fy
 end
-function gradient!(grads::ArrayPartition, g::SeparableSum, xs::ArrayPartition)
-    _, f_val = gradient!(grads.x, g, xs.x)
-    return grads, f_val
-end
+gradient!(grads::ArrayPartition, g::SeparableSum, xs::ArrayPartition) = gradient!(grads.x, g, xs.x)
 function gradient(g::SeparableSum, xs::ArrayPartition)
     grad, f_val = gradient(g, xs.x)
     return ArrayPartition(grad), f_val
